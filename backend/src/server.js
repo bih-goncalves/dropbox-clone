afd862175febed5c43f43036c0d8f9091dc65bbe
bih-goncalves <bih_goncalves@hotmail.com>
2019-04-19 23:34:10 -0300

    Backend - creating storage model

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require("mongoose");
 
 const app = express();
 
+// connecting with database
+mongoose.connect(
+    "mongodb+srv://omnistack:backend@cluster0-voj6e.mongodb.net/test?retryWrites=true",
+    {
+        useNewUrlParser: true
+    }
+);
+
 app.use(express.json());
-app.use(express.urlencoded({extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(require('./routes'));
 
