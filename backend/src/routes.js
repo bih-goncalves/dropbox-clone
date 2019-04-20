7e9c2b0b3d34368a422ff2eab19e986dda8c0cd9
bih-goncalves <bih_goncalves@hotmail.com>
2019-04-19 22:02:19 -0300

    Backend - inserting auto run config

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
+
 const routes = express.Router();
 
 routes.get('/teste', (req, res) => {
-    return res.send("Hello world!");
-})
+    return res.send('Hello Rocket');
+});
 
 module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true }));
 
 app.use(require('./routes'));
+
 app.listen(4200);
