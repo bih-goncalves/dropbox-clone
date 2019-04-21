c782ec621e673ce4aaa6730368c6371c93ce51b8
bih-goncalves <bih_goncalves@hotmail.com>
2019-04-20 21:02:10 -0300

    changing post for files

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -3,7 +3,7 @@ const File = require("../models/File");
 
 class FileController {
     async store (req, res) {
-        const box = await Box.find({title:req.params.title});
+        const box = await Box.findOne({title:req.params.title});
         
         const file = await File.create({
             title: req.file.originalname,
@@ -14,7 +14,7 @@ class FileController {
 
         await box.save();
         // alert everyone that a new file was created
-        req.io.sockets.in(box._id).emit('file', file);
+        req.io.sockets.in(box.title).emit('file', file);
         // creating file
         return res.json(file);
     }
